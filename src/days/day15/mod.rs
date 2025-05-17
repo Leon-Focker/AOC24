1e1c9119f089d159382efecde88175414f6268f0
Leon-Focker <Leon.Focker@t-online.de>
2025-05-17 15:51:52 +0200

    traits and structs for Large and Normale Warehouses.

--- src/days/day15/mod.rs
+++ src/days/day15/mod.rs
@@ -3,43 +3,115 @@ const DAY: u32 = 15;
 
 pub fn run(debug: bool) {
     let lines = load_input(DAY, debug);
-    let (mut warehouse, mut moves) = parse_input(&lines);
+    let (mut normal_warehouse, mut large_warehouse, mut moves) = parse_input(&lines);
 
-    let result_part1 = part1(&mut warehouse, &mut moves);
-    let result_part2 = part2(&mut warehouse, &mut moves);
+    let result_part1 = part1(&mut normal_warehouse, &mut moves);
+    let result_part2 = part2(&mut large_warehouse, &mut moves);
 
     println!("Day {}, Part 1: {}", DAY, result_part1);
     println!("Day {}, Part 2: {}", DAY, result_part2);
 }
 
-// 0 -> wall, 1 -> none, 2 -> box, 3 -> robot
+// Shared data struct for common fields and logic
 #[derive(Debug)]
-struct Warehouse {
+struct WarehouseData {
     map: Vec<Vec<usize>>,
     width: usize,
     height: usize,
     robot: (usize, usize),
 }
 
-impl Warehouse {
+impl WarehouseData {
     fn new() -> Self {
-        Warehouse { map: Vec::new(), width: 0, height: 0, robot: (0, 0) }
+        WarehouseData { map: Vec::new(), width: 0, height: 0, robot: (0, 0) }
     }
-
-    fn update_size(&mut self) -> () {
+    fn update_size(&mut self) {
         self.width = self.map[0].len();
         self.height = self.map.len();
     }
 
-    fn set_robot(&mut self) -> () {
+    fn init_robot(&mut self) {
         for y in 1..self.height {
             for x in 1..self.width {
                 if self.map[y][x] == 3 {
                     self.robot = (x, y);
-                    break
+                    return;
                 }
             }
         }
+    }
+
+    fn gps_sum(&self, look_for: usize) -> usize {
+        let mut result = 0;
+
+        for y in 1..self.height {
+            for x in 1..self.width {
+                if self.map[y][x] == look_for {
+                    result += 100 * y + x;
+                }
+            }
+        }
+
+        result
+    }
+}
+
+// Trait providing shared methods
+trait Warehouse {
+    fn data(&self) -> &WarehouseData;
+    fn data_mut(&mut self) -> &mut WarehouseData;
+
+    fn update_size(&mut self) {
+        self.data_mut().update_size();
+    }
+
+    fn init_robot(&mut self) {
+        self.data_mut().init_robot();
+    }
+
+    fn update(&mut self, moves: &mut Moves) -> bool;
+    fn space_behind(&self, mv: &Move, x: usize, y: usize);
+
+    fn draw(&self) -> () {
+        for y in 0..self.data().height {
+            println!();
+            for x in 0..self.data().width {
+                let res = self.data().map[y][x];
+
+                match res {
+                    0 => print!("#"),
+                    1 => print!("."),
+                    2 => print!("O"),
+                    4 => print!("["),
+                    5 => print!("]"),
+                    _ => print!("@"),
+                }
+            }
+        }
+        println!();
+    }
+
+    fn gps_sum(&self) -> usize;
+}
+
+#[derive(Debug)]
+struct NormalWarehouse {
+    data: WarehouseData,
+}
+
+impl NormalWarehouse {
+    fn new() -> Self {
+        Self { data: WarehouseData::new() }
+    }
+}
+
+impl Warehouse for NormalWarehouse {
+    fn data(&self) -> &WarehouseData {
+        &self.data
+    }
+
+    fn data_mut(&mut self) -> &mut WarehouseData {
+        &mut self.data
     }
 
     fn update(&mut self, moves: &mut Moves) -> bool {
@@ -48,24 +120,24 @@ impl Warehouse {
         if *next_move == Move(0, 0) {
             false
         } else {
-            let (rx, ry) = self.robot;
+            let (rx, ry) = self.data.robot;
             let (x, y) = ((rx as isize + next_move.0) as usize, (ry as isize + next_move.1) as usize);
-            let next_block = self.map[y][x];
+            let next_block = self.data.map[y][x];
 
             match next_block {
                 0 => (),
                 1 => {
-                    self.robot = (x, y);
-                    self.map[y][x] = 3;
-                    self.map[ry][rx] = 1;
+                    self.data.robot = (x, y);
+                    self.data.map[y][x] = 3;
+                    self.data.map[ry][rx] = 1;
                 },
                 _ => {
-                    let (space_x, space_y) = self.space_behind(next_move, (x, y));
+                    let (space_x, space_y) = self.space_behind(next_move, x, y);
                     if (space_x, space_y) != (0, 0) {
-                        self.robot = (x, y);
-                        self.map[y][x] = 3;
-                        self.map[ry][rx] = 1;
-                        self.map[space_y][space_x] = 2;
+                        self.data.robot = (x, y);
+                        self.data.map[y][x] = 3;
+                        self.data.map[ry][rx] = 1;
+                        self.data.map[space_y][space_x] = 2;
                     }
                 },
             }
@@ -73,10 +145,10 @@ impl Warehouse {
         }
     }
 
-    fn space_behind(&self, mv: &Move, (mut x, mut y): (usize, usize)) -> (usize, usize) {
+    fn space_behind(&self, mv: &Move, mut x: usize, mut y: usize) -> (usize, usize) {
         loop {
             (x, y) = ((x as isize + mv.0) as usize, (y as isize + mv.1) as usize);
-            let block = self.map[y][x];
+            let block = self.data.map[y][x];
 
             match block {
                 1 => return (x, y),
@@ -87,33 +159,71 @@ impl Warehouse {
     }
 
     fn gps_sum(&self) -> usize {
-        let mut result = 0;
+        self.data.gps_sum(2)
+    }
+}
 
-        for y in 1..self.height {
-            for x in 1..self.width {
-                if self.map[y][x] == 2 {
-                    result += 100 * y + x;
-                }
-            }
-        }
+#[derive(Debug)]
+struct LargeWarehouse {
+    data: WarehouseData,
+}
 
-         result
+impl LargeWarehouse {
+    fn new() -> Self {
+        Self { data: WarehouseData::new() }
+    }
+}
+
+impl Warehouse for LargeWarehouse {
+    fn data(&self) -> &WarehouseData {
+        &self.data
     }
 
-    fn draw(&self) -> () {
-        for y in 0..self.height {
-            println!();
-            for x in 0..self.width {
-                let res = self.map[y][x];
+    fn data_mut(&mut self) -> &mut WarehouseData {
+        &mut self.data
+    }
 
-                match res {
-                    0 => print!("#"),
-                    1 => print!("."),
-                    2 => print!("O"),
-                    _ => print!("@"),
-                }
+    fn update(&mut self, moves: &mut Moves) -> bool {
+        let next_move = moves.next();
+
+        if *next_move == Move(0, 0) {
+            false
+        } else {
+            let (rx, ry) = self.data.robot;
+            let (x, y) = ((rx as isize + next_move.0) as usize, (ry as isize + next_move.1) as usize);
+            let next_block = self.data.map[y][x];
+
+            match next_block {
+                0 => (),
+                1 => {
+                    self.data.robot = (x, y);
+                    self.data.map[y][x] = 3;
+                    self.data.map[ry][rx] = 1;
+                },
+                // todo 4 -> linke seite, 5 -> rechte Seite.
+                // todo wie dann so viele boxen schieben??
+                _ => {
+                    let (space_x, space_y) = self.space_behind(next_move, x, y);
+                    if (space_x, space_y) != ((0, 0), (0, 0)) {
+                        self.data.robot = (x, y);
+                        self.data.map[y][x] = 3;
+                        self.data.map[ry][rx] = 1;
+                        self.data.map[space_y][space_x] = 2;
+                    }
+                },
             }
+            true
         }
+    }
+
+    fn space_behind(&self, _mv: &Move, _x: usize, _y: usize) -> ((usize, usize), (usize, usize)) {
+        todo!()
+        // for left and right same as normalWarehouse, up and down need to check for two spaces next to each other
+        // if a 5 is above/below a 4 and vice versa, check for 2 boxes (recursively)...
+    }
+
+    fn gps_sum(&self) -> usize {
+        self.data.gps_sum(4)
     }
 }
 
@@ -141,8 +251,9 @@ impl Moves {
     }
 }
 
-fn parse_input(lines: &Vec<String>) -> (Warehouse, Moves) {
-    let mut warehouse = Warehouse::new();
+fn parse_input(lines: &Vec<String>) -> (NormalWarehouse, LargeWarehouse, Moves) {
+    let mut normal_warehouse = NormalWarehouse::new();
+    let mut large_warehouse = LargeWarehouse::new();
     let mut moves = Moves::new();
     let mut warehouse_done = false;
 
@@ -150,19 +261,37 @@ fn parse_input(lines: &Vec<String>) -> (Warehouse, Moves) {
 
         if !warehouse_done {
             let mut row = Vec::with_capacity(line.len());
+            let mut large_row = Vec::new();
 
             for char in line.chars() {
                 match char {
-                    '#' => row.push(0),
-                    '.' => row.push(1),
-                    'O' => row.push(2),
-                    '@' => row.push(3),
+                    '#' => {
+                        row.push(0);
+                        large_row.push(0);
+                        large_row.push(0);
+                    },
+                    '.' => {
+                        row.push(1);
+                        large_row.push(1);
+                        large_row.push(1);
+                    },
+                    'O' => {
+                        row.push(2);
+                        large_row.push(4);
+                        large_row.push(5);
+                    },
+                    '@' => {
+                        row.push(3);
+                        large_row.push(3);
+                        large_row.push(1);
+                    },
                     _ => warehouse_done = true,
                 }
             }
 
             if row.len() > 0 {
-                warehouse.map.push(row);
+                normal_warehouse.data.map.push(row);
+                large_warehouse.data.map.push(large_row);
             }
         }
         if warehouse_done {
@@ -184,27 +313,31 @@ fn parse_input(lines: &Vec<String>) -> (Warehouse, Moves) {
         }
     }
 
-    warehouse.update_size();
-    warehouse.set_robot();
+    normal_warehouse.update_size();
+    normal_warehouse.init_robot();
+    large_warehouse.update_size();
+    large_warehouse.init_robot();
 
-    (warehouse, moves)
+    (normal_warehouse, large_warehouse, moves)
 }
 
 
-fn part1(warehouse: &mut Warehouse, moves: &mut Moves) -> usize {
+fn part1(normal_warehouse: &mut NormalWarehouse, moves: &mut Moves) -> usize {
 
     loop {
-        //warehouse.draw();
-        if !warehouse.update(moves) {
+        //normal_warehouse.draw();
+        if !normal_warehouse.update(moves) {
             break
         }
     }
 
-    warehouse.gps_sum()
+    normal_warehouse.gps_sum()
 }
 
-fn part2(_warehouse: &mut Warehouse, _moves: &mut Moves) -> usize {
+fn part2(large_warehouse: &mut LargeWarehouse, _moves: &mut Moves) -> usize {
     let result = 0;
+
+    large_warehouse.draw();
 
     result
 }
